e1938e2d54ea9113acaab21731ad22131fa51082
jhyunwoo <jhyunwoo0228@gmail.com>
2022-09-18 16:04:18 +0900

    try to fix prisma create error

---
+++ pages/api/post.js
@@ -0,0 +1,11 @@
+import prisma from "../../libs/prisma";
+export default async function handle(req, res) {
+  const { img_url, authorEmail } = req.body;
+  const result = await prisma.post.create({
+    data: {
+      img_url: img_url,
+      author: { connect: { email: authorEmail } },
+    },
+  });
+  res.json(result);
+}
