3568ec4bd1e0109daf4948fa127c80f4d48ad7c9
jhyunwoo <jhyunwoo0228@gmail.com>
2022-09-22 01:34:37 +0900

    finish main page

--- pages/index.js
+++ pages/index.js
@@ -1,81 +1,139 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-
 import prisma from "../libs/prisma";
-import {useRouter} from "next/router";
 import Link from "next/link";
-
 
 export const getServerSideProps = async () => {
   const posts1 = await prisma.post.findMany({
-      include: {
-          like: true,
-      }
-    }
-  )
-  const posts = JSON.stringify(posts1)
-  return { props: { posts } }
-}
+    include: {
+      like: true,
+      author: true,
+    },
+  });
+  const posts = JSON.stringify(posts1);
+  return { props: { posts } };
+};
 
-export default function Main({posts}) {
+export default function Main({ posts }) {
   const { data: session } = useSession();
-  const postData = JSON.parse(posts)
-    const router = useRouter()
-    async function deletePost(){
-        const postId = post.id
-        await fetch(`/api/deletePost`, {
-            method: "DELETE",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({postId}),
-        })
-        await router.replace(router.asPath)
-    }
-    if (session) {
+
+  //serverside에서 받은 데이터 json으로 변환
+  const postData = JSON.parse(posts);
+
+  // Post 삭제 function
+  async function deletePost(postId) {
+    await fetch(`/api/deletePost`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ postId }),
+    });
+  }
+
+  async function addLike(userEmail, postId) {
+    await fetch(`/api/addLike`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userEmail, postId }),
+    });
+  }
+  if (session) {
     return (
-      <div>
-        <div>{session.user.name}님 안녕하세요</div>
-        <div onClick={() => signOut()}>로그아웃</div>
-          <Link href={"/post"}>
-              <div>Post page</div>
-          </Link>
-        <div>
-          <div>
-            {postData.map((post, key) => (
-              <div key={key}>
-                <img src={`https://imagedelivery.net/p4TahxOLmTmIsMIJ7PpE9A/${post.img_url}/public`}/>
-                <div>{post.title}</div>
-                  <button onClick={async ()=>{
-                      const userEmail = session.user.email
-                        const postId = post.id
-                      await fetch(`/api/addLike`, {
-                          method: "POST",
-                          headers: { "Content-Type": "application/json" },
-                          body: JSON.stringify({userEmail, postId}),
-                      })
-                      await router.replace(router.asPath)
-                  }}>add like</button>
-                  <button onClick={async ()=>{
-                      const postId = post.id
-                      await fetch(`/api/deletePost`, {
-                          method: "DELETE",
-                          headers: { "Content-Type": "application/json" },
-                          body: JSON.stringify({postId}),
-                      })
-                      await router.replace(router.asPath)
-                  }}>delete post</button>
-                  <div>{post.like.length}</div>
-
+      <div className="bg-slate-100 w-screen">
+        <div className="flex justify-between p-4">
+          <div className="text-4xl font-bold">CNAlbum</div>
+          <div className="flex">
+            <div className="my-auto ">
+              <div
+                className="m-2 bg-blue-500 hover:bg-blue-600 p-2 rounded-lg text-white"
+                onClick={() => {
+                  if (session) {
+                    signOut();
+                  } else {
+                    signIn();
+                  }
+                }}
+              >
+                {session ? "로그아웃" : "로그인"}
               </div>
-
-            ))}
+            </div>
+            <Link href={"/post"}>
+              <div className="my-auto bg-blue-500 hover:bg-blue-600 p-1 rounded-lg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8 text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                  />
+                </svg>
+              </div>
+            </Link>
           </div>
         </div>
+        {postData.map((data, key) => (
+          <div key={key}>
+            <div className="bg-white m-4 w-72 mx-auto rounded-2xl flex flex-col">
+              <div className="h-14 flex">
+                <div className="my-auto mx-6 text-lg">{data.author.name}</div>
+              </div>
+              <div
+                className="w-72 h-72 bg-gray-200 bg-cover bg-center"
+                style={{
+                  backgroundImage: `url(
+                  https://imagedelivery.net/p4TahxOLmTmIsMIJ7PpE9A/${data.img_url}/public
+                )`,
+                }}
+              ></div>
+              <div className="h-20">
+                <div className="flex my-2 mx-3">
+                  <div
+                    className="my-auto"
+                    onClick={() => addLike(session.user.email, data.id)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-7 h-7"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                      />
+                    </svg>
+                  </div>
+                  <div className="mx-2 my-auto">{data.like.length} likes</div>
+                </div>
+                <div className=" mx-4">
+                  <div>{data.title}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+        <div className="w-full h-10"></div>
       </div>
     );
   }
   return (
-    <div>
-      <div>로그인 필요</div>
-      <div onClick={() => signIn()}>로그인 하러가기</div>
+    <div className="w-screen h-screen flex">
+      <div className="mx-auto my-auto flex flex-col">
+        <div className="text-6xl font-semibold">CNAlbum</div>
+        <div
+          className="mx-auto my-4 bg-blue-500 text-white p-2 px-4 rounded-xl hover:bg-blue-600 transition"
+          onClick={() => signIn()}
+        >
+          로그인
+        </div>
+      </div>
     </div>
   );
 }
--- pages/post.js
+++ pages/post.js
@@ -1,47 +1,55 @@
 import { useForm } from "react-hook-form";
-import {useSession} from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-export default function Post(){
-    const { data: session } = useSession();
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
+export default function Post() {
+  const { data: session } = useSession();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const router = useRouter();
 
-    async function transferPost(data){
-        if (data.image && data.image.length > 0) {
-            const {uploadURL} = await (await fetch(`/api/uploadImages`)).json()
-            const form = new FormData();
-            form.append("file", data.image[0]);
-            const request = await (await fetch(uploadURL, {
-                method: "POST",
-                body: form,
-            })).json()
-            console.log(request.result.id)
-            const img_url = request.result.id
-            const title = data.title
-            const authorEmail = session.user.email
-            const body = {img_url, authorEmail, title };
-            await fetch(`/api/createPost`, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(body),
-            }).then((e)=>console.log(e))
-        }
-        console.log(data)
+  async function transferPost(data) {
+    if (data.image && data.image.length > 0) {
+      const { uploadURL } = await (await fetch(`/api/uploadImages`)).json();
+      const form = new FormData();
+      form.append("file", data.image[0]);
+      const request = await (
+        await fetch(uploadURL, {
+          method: "POST",
+          body: form,
+        })
+      ).json();
+      console.log(request.result.id);
+      const img_url = request.result.id;
+      const title = data.title;
+      const authorEmail = session.user.email;
+      const body = { img_url, authorEmail, title };
+      await fetch(`/api/createPost`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      }).then((e) => console.log(e));
     }
+    router.push("/");
+  }
 
-        return (
-        <div>
-            <div>Post page</div>
-            <form onSubmit={handleSubmit(transferPost)}>
-                <input {...register("image", {
-                    required: "이미지를 업로드하세요.",
-                })} type={"file"} accept="image/*" />
-                <input {...register("title")} type={"text"} placeholder={"title"}/>
-                <button type={"submit"}>Submit</button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <div>Post page</div>
+      <form onSubmit={handleSubmit(transferPost)}>
+        <input
+          {...register("image", {
+            required: "이미지를 업로드하세요.",
+          })}
+          type={"file"}
+          accept="image/*"
+        />
+        <input {...register("title")} type={"text"} placeholder={"title"} />
+        <button type={"submit"}>Submit</button>
+      </form>
+    </div>
+  );
 }
