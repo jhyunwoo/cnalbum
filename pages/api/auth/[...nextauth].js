c9eea4165a61c0db2a50a13519da63beb0d2d6e3
jhyunwoo <jhyunwoo0228@gmail.com>
2022-09-23 03:20:50 +0900

    give up kakao login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,10 +13,10 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET,
-    }),
+    // KakaoProvider({
+    //   clientId: process.env.KAKAO_CLIENT_ID,
+    //   clientSecret: process.env.KAKAO_CLIENT_SECRET,
+    // }),
   ],
   secret: process.env.SECRET,
 });
